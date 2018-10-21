e6d2ca1d94b4bd6c75f7ec7d684a7282bd4e6f89
Mauricio Reyna <mauriciojreyna@gmail.com>
2018-10-20 20:32:32 -0400

    comments added

--- Lab9/Program.cs
+++ Lab9/Program.cs
@@ -62,27 +62,27 @@ namespace Lab8
                 else if (input.Equals("favorite food", StringComparison.InvariantCultureIgnoreCase))
                 { //if user inputs "favorite food" display name and food
                     Console.WriteLine("{0}'s favorite food is {1} ", names[num - 1], food[num - 1]);
-                }//People[num - 1,0]... displays row and columns of the students and their information\
+                }
                 else
-                {
+                { //display name and favorite color
                     Console.WriteLine("{0}'s favorite color is {1} ", names[num - 1], color[num - 1]);
                 }
 
                 Console.WriteLine("Would you like more info on students? If so write one of these. (student, add, exit)");
                 input = Console.ReadLine();
 
-                if (input.Equals("student", StringComparison.InvariantCultureIgnoreCase)) //if the input reads "yes" continue
+                if (input.Equals("student", StringComparison.InvariantCultureIgnoreCase)) //seeing if the user enters "student" to learn aobut more students
                 {
                     shouldContinue = true;
                 }
-                else if (input.Equals("add", StringComparison.InvariantCultureIgnoreCase))
+                else if (input.Equals("add", StringComparison.InvariantCultureIgnoreCase)) //seeing if the user enters "add" to add a student and his info
                 {
                     AddStudent();
                     shouldContinue = true;
                 }
                 else
                 {
-                    Console.WriteLine("Goodbye Buddy!");
+                    Console.WriteLine("Goodbye!"); //ends program
                 }
             } while (shouldContinue);
             Console.ReadKey();
@@ -93,16 +93,16 @@ namespace Lab8
         static List<string> names = new List<string> { "Adriana", "Milton", "Marilyn", "Andrew", "John", "Johnny", "Keith", "Chaz", "Blake", "Richard", "Julia", "Bill", "Lous", "Zoey", "Francis", "Rochelle", "Nick", "Ellis", "Darrel" };
         static List<string> hometown = new List<string> { "New York", "Atlanta", "Balitmore", "Memphis", "Las Vegas", "AUstin", "Bismarck", "Los Angeles", "Detroit", "Boston", "El Pas", "San Antonio", "Buffalo", "Newark", "Seattle", "St. Louis", "San Fransico", "Kanas City", "Chicago" };
         static List<string> food = new List<string> { "French Toast", "Biscuts and Gravy", "Fish", "Grits", "Tacos", "T-Bone Steak", "Fries", "Salad", "Bratwurst", "Scallops", "Chicken Wings", "BBQ", "Tiramisu", "Spring Roll", "Quiche", "Chicken Nuggets", "Dulce De Leche", "Waffles", "Hot Dogs" };
-        static List<string> color = new List<string> { "Green", "Purple", "Orange", "Blue", "Yellow", "Red", "Navy Blue", "Lime Green", "Gray", "Black", "White", "Brick Red", "Olive", "Dark Violet", "Pale Green", "Gold", "Cyan", "Maroon", "Magenta"};
+        static List<string> color = new List<string> { "Green", "Purple", "Orange", "Blue", "Yellow", "Red", "Navy Blue", "Lime Green", "Gray", "Black", "White", "Brick Red", "Olive", "Dark Violet", "Pale Green", "Gold", "Cyan", "Maroon", "Magenta"}; //the whole enchilada of student info
 
-        static string GetUserInput(string inputName)
+        static string GetUserInput(string inputName) //method
         {
             string input;
 
             bool isValidInput = false;
             do
             {
-                isValidInput = true;
+                isValidInput = true;//if input is valid it's true
 
                 Console.WriteLine("Please enter {0}", inputName);
                 input = Console.ReadLine();
@@ -112,13 +112,13 @@ namespace Lab8
                     Console.WriteLine("Please input valid info");
                     isValidInput = false;
                 }
-            } while (!isValidInput);
+            } while (!isValidInput);//if input is not valid do this again until it is valid
             return input;
         }
-        static void AddStudent()
+        static void AddStudent() //adding a student to the list
         {
             names.Add(GetUserInput("name"));
-            hometown.Add(GetUserInput("hometown"));
+            hometown.Add(GetUserInput("hometown"));//the student is added to list by x.Add(GetUserInput("x"));
             food.Add(GetUserInput("food"));
             color.Add(GetUserInput("color"));
         }
